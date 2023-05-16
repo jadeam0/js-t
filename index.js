cd36f9e327ef5559fd5698117e893af5c76afc1c
jadeam0 <jdpaulse27@gmail.com>
2023-05-16 08:53:36 +0200

    js slide task

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    alert ("Hello! My name is Jadee!!");
+}
+
+console.log ('Hello World');
+2+2;
+alert ('This is the browsers alert box');
+
+let name='Jadee Paulse';
+console.log ('Jadee Paulse')
